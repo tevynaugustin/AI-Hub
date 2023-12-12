7049c105c6361659cd86ec4abceb57b42025fbcc
Tevyn Augustin <tevynaugustin@gmail.com>
2023-12-12 11:29:34 +0000

    more stuff added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,9 @@ import { getAuth } from "firebase/auth";
 import { NavLink, useNavigate } from "react-router-dom";
 import "./Home.css";
 import Navbar from "../navbar/navbar";
+import Typewriter from 'typewriter-effect';
+import bgImg from './landing-video.mp4';
+
 
 function Home() {
   const auth = getAuth();
@@ -23,15 +26,33 @@ function Home() {
     <div>
       <Navbar />
       <div className="home-body">
-        <h1 className="welcome-msg">
-          Hey, <span className="displayName">{displayName}</span>
+        <div className="landing-photo">
+          <video autoPlay loop muted style={{
+            objectFit: "cover",
+            height: "425px",
+            width: "100%",
+            opacity: "0.5",
+            borderBottom: "5px solid #0080ff"
+          }}>
+            <source src={bgImg} type="video/mp4"/>
+            Your Browser does not support the video tag
+          </video>
+        </div>
+      <h1 className="welcome-msg">
+      <Typewriter onInit={(typewriter) => {
+  typewriter.typeString(`Hey, `)
+    .start()
+    .typeString(`<span class="displayName">${displayName}</span>!`)
+    .start();
+}} />
+
         </h1>
         <p className="introduction">
           Unleash your inner geek with
-          $name$, your daily dose of tech tales! From giants like Google and
+          DataZ, your daily dose of tech tales! From giants like Google and
           Apple to David-and-Goliath startups, we've got the scoop on the
           coolest gadgets, quirkiest apps, and everything in between. Join the
-          nerdy fun at $name$ because tech news doesn't have to be serious!
+          nerdy fun at DataZ because tech news doesn't have to be serious!
         </p>
       </div>
     </div>
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -28,68 +28,82 @@ function Signup() {
       .catch((error) => {
         console.log(error);
 
-        if (error.code === "auth/weak-password") {
-          setErrorMessage("Password should be at least 6 characters");
-        } else if (error.code === "auth/email-already-in-use") {
-          setErrorMessage("This email is already in use");
-        } else {
-          setErrorMessage("An error occurred. Please try again.");
+        switch (error.code) {
+          case "auth/weak-password":
+            setErrorMessage("Password should be at least 6 characters");
+            break;
+          case "auth/email-already-in-use":
+            setErrorMessage("This email is already in use");
+            break;
+          default:
+            setErrorMessage("An error occurred. Please try again.");
+            break;
+          case "auth/invalid-display-name":
+            setErrorMessage("Please enter a display name.");
+            break;
+          case "auth/invalid-email":
+            setErrorMessage("Email address is Invalid!");
+            break;
         }
-      }).finally(() => {
+      })
+      .finally(() => {
         setLoading(false);
       });
   };
 
   return (
     <div className="sign-up-body">
-    <div className="sign-up-container">
-      <form onSubmit={handleSignUp}>
-      <label htmlFor="displayName">Enter your display name</label>
-        <input
-          required
-          type="text"
-          placeholder="Enter your display name"
-          id="displayName"
-          value={displayName}
-          onChange={(e) => {
-            setDisplayName(e.target.value);
-          }}
-        />
+      <div className="sign-up-container">
+        <div className="sign-up-header">
+          <h2 className="sign-up-title">Sign Up</h2>
+        </div>
+        <form onSubmit={handleSignUp}>
+          <label htmlFor="displayName">Enter your display name</label>
+          <input
+            required
+            type="text"
+            placeholder="Enter your display name"
+            id="displayName"
+            value={displayName}
+            onChange={(e) => {
+              setDisplayName(e.target.value);
+            }}
+          />
 
-        <label htmlFor="email">Enter an email</label>
-        <input
-          required
-          type="email"
-          placeholder="Enter an email"
-          id="email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
+          <label htmlFor="email">Enter an email</label>
+          <input
+            required
+            type="email"
+            placeholder="Enter an email"
+            id="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
 
-        <label htmlFor="password">Enter a password</label>
-        <input
-          required
-          type="password"
-          placeholder="Enter a password"
-          id="password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
+          <label htmlFor="password">Enter a password</label>
+          <input
+            required
+            type="password"
+            placeholder="Enter a password"
+            id="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
 
-        <button type="submit">Sign up</button>
+          <button type="submit">Sign up</button>
 
-        <p>
-          Or <NavLink to={"/Signin"}>Sign in</NavLink>
-        </p>
+          <p>
+            Or <NavLink to={"/Signin"}>Sign in</NavLink>
+          </p>
 
-        {errorMessage && <p className="error-message">{errorMessage}</p>}
-        {loading && <p className="loading"></p>}
-      </form>
-    </div>
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
+          {loading && <p className="loading"></p>}
+        </form>
+      </div>
     </div>
   );
 }
